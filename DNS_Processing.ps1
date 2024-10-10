b318aebcef74ec34a2625116c3052a98b994672f
mallengoebel <matt.allengoebel@jbhunt.com>
2024-10-10 11:37:00 -0500

    anonymizing DNS_Processing.ps1

--- DNS_Processing.ps1
+++ DNS_Processing.ps1
@@ -1,7 +1,7 @@
 $ips = @( "ip1", "ip2", "ip3", "ip4" )
 foreach ( $ip in $ips ) {
 	$result = nslookup $ip
-	$name = ( $result | Select-String "Name:").Line -replace "Name:\s+", "" -replace "\.jbhunt\.com$", ""
+	$name = ( $result | Select-String "Name:").Line -replace "Name:\s+", "" -replace "\.example\.com$", ""
 	get-adcomputer $name
 	}
 	
