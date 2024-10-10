d95d6c73a23331a7946603347930c00016d03fba
mallengoebel <matt.allengoebel@jbhunt.com>
2024-10-10 11:40:44 -0500

    Anonymizing Update Help-section.ps1

--- Help-section.ps1
+++ Help-section.ps1
@@ -11,30 +11,30 @@ Usage:	.\help-section.ps1 [options]
 
 Options:
 	-help			Display this help message
-	-extpurge		Roll the service for External Purge on 10306
+	-extpurge		Roll the service for <Service Name>
 	-cleandump <value>	Clean up the spurious dump files on one of three servers
 
 
 Examples:
   .\help-section.ps1 -help
   .\help-section.ps1 -extpurge
-  .\help-section.ps1 -cleandump jvpb2b10306
-  .\help-section.ps1 -extpurge -cleandump jvpb2b10306
+  .\help-section.ps1 -cleandump <servername>
+  .\help-section.ps1 -extpurge -cleandump <servername>
  
 
 Description:
-  This is a script which is designed to be a one stop shop for easily remedied B2B cluster issues.
-  The first is an external purge service roll. The second is a clean up of dump files for disk
+  This is a script which is designed to be a one stop shop for easily remedied issues.
+  The first is an <servicename> service roll. The second is a clean up of dump files for disk
   space issues. Other tasks can be added.
 "@
 
 # Function to roll the external purge service running on 10306
 function ext-purge-roll {
     $Server = "Some-server"
-    $Service = "IBM*Ext*Purge*8080"
+    $Service = "<servicename>"
     $marker = 1
     if ($marker -eq 1) {
-        Write-Host "Parameters Set for External Purge Service Roll"
+        Write-Host "Parameters Set for <Service Name> Service Roll"
         Write-Host "Server:" $Server
         Write-Host "Service:" $Service
     }
